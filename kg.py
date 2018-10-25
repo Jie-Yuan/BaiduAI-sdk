7b8e7fc3f3f44df0a9ab96f2ef36774a9c5edf08
尐 <313303303@qq.com>
2018-10-25 16:49:49 +0800

    Update kg.py

--- kg.py
+++ kg.py
@@ -4,17 +4,24 @@ import urllib
 import requests
 
 
+import json
+import urllib
+
+import requests
+
+
 class BaiDuNLP(object):
     def __init__(self, api_key='B0cphM43tmxUyR4YcGMvCn1X', secret_key='SjRpd3Fg6aaX9B5KQD796IDHgvFMrURb'):
+        self.access_token = None
         self._get_access_token(api_key, secret_key)
 
-    def get_api_prediction(self, text, api_url='/v1/lexer'):
-        """http://ai.baidu.com/docs#/NLP-API/89828646
-        api_url:
-            /v1/lexer: 词法分析接口
-            /v1/depparser: 依存句法分析接口
+    def predict(self, text, api_url):
         """
-        url = f'https://aip.baidubce.com/rpc/2.0/kg/v1/cognitive/entity_annotation?charset=UTF-8&access_token=' + self.access_token
+        api_url = 'https://aip.baidubce.com/rpc/2.0/kg/v1/cognitive/entity_annotation?charset=UTF-8&access_token='
+        api = BaiDuNLP()
+        api.predict('周杰伦', api_url)
+        """
+        url = api_url + self.access_token
         
         # the input is json format
         input_text = {'data': text}
